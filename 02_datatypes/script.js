40a271cb16fad451379af5ab7de9bd5a4ab51834
Ankit Yadav <khushhal2112@gmail.com>
2024-03-01 18:09:24 +0000

    container configure done

---
+++ 02_datatypes/script.js
@@ -0,0 +1,25 @@
+console.log("jai shree ram");
+console.log("jai shree ram");
+
+//number datatypes
+let age = 89;
+
+//boolean datatypes 
+let isHappy =true; 
+
+//strings datatypes
+let lastName = "Yadav"; 
+
+//convert number into string
+let empty = 100 + ''; 
+
+//converting into number datatypes
+let ishappy = +true; 
+
+//bigInt datatype
+const previouslyMaxSafeInteger = 9007199254740991n;
+
+//Symbol datatypes
+const sym1 = Symbol();
+const sym2 = Symbol("foo");
+const sym3 = Symbol("foo");
