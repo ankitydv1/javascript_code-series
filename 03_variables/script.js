3879693ab4ce45f9e59f8b4b6aeac1e5239cf390
Ankit Yadav <khushhal2112@gmail.com>
2024-03-02 18:16:54 +0000

    config done

---
+++ 03_variables/script.js
@@ -0,0 +1,30 @@
+//varibales using let 
+
+//strings variable
+let myVariable = 'ankit';
+let userName = 'anur';
+let lastName = 'Yadav.'
+//numbers variable
+let age = 25;
+
+//change the variable 
+userName = 'ankit';
+
+let userIntr ='Hi I am '+ userName + ' ' + lastName + ' ' + ' I am ' + age + ' years old';
+
+//boolean variable
+let isHappy = true;
+console.log(userName);
+let a = undefined;
+
+const hoursDay = 24; // we cannot change the variable value later, it is constant variable
+
+
+var name = 'javascript';
+
+var p 
+
+// we use only underscore and dollar variable name representaion
+
+let user_name = 'ankit';
+let old$age = 24;
