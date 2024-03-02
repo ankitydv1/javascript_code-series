e50945590f1ea69d7423ed9520e2cded4e3f0995
Ankit Yadav <khushhal2112@gmail.com>
2024-03-02 21:55:05 +0000

    container config done

---
+++ 04_howCodeRun/script.js
@@ -0,0 +1,9 @@
+console.log(firstName);
+console.log(lastName); //error
+
+var firstName = 'ankit';
+let lastName = 'Yadav';
+let age = 15;
+const yearOfbirth = 2005;
+
+let userIntro = 'hi, My name is ' + firstName + ' ' + lastName;
