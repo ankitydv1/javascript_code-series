9c21501c21f0e522825e7c4aa39a02feb3f8c5a1
Ankit Yadav <khushhal2112@gmail.com>
2024-03-03 17:13:06 +0000

    container  config done

---
+++ 05_dialogBoxes/script.js
@@ -0,0 +1,12 @@
+//alert
+alert("your goes here");
+
+//confirm
+confirm("would you like to proceed");
+const isConfirmed = confirm("would you like to proceed");
+console.log()
+
+// prompt
+ prompt("enter the name");
+ const userInput = prompt("enter your name");
+ console.log(userInput);
