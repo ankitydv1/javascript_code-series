ac4a547e3d3d644c95033d6c39d34fec4ddbf44b
Ankit Yadav <khushhal2112@gmail.com>
2024-03-06 10:15:20 +0000

    container config done

---
+++ 06_stringMethod/script.js
@@ -0,0 +1,7 @@
+const message = 'hello world';
+const capitalMessage = message.toUpperCase()
+
+const xtraMessage = '  hi, I am Anurag  ';
+const trimMessage = xtraMessage.trim();
+const trimEnd = xtraMessage.trimEnd();
+const trimStart = xtraMessage.trimStart();
