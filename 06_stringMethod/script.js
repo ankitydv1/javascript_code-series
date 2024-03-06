7d936f2a627907b80ea98c324fcbeab9c24453c5
Ankit Yadav <khushhal2112@gmail.com>
2024-03-06 20:58:18 +0000

    container config done

--- 06_stringMethod/script.js
+++ 06_stringMethod/script.js
@@ -5,3 +5,19 @@ const xtraMessage = '  hi, I am Anurag  ';
 const trimMessage = xtraMessage.trim();
 const trimEnd = xtraMessage.trimEnd();
 const trimStart = xtraMessage.trimStart();
+
+
+const rawMessage = ' hey! I am Ankit Yadav';
+const finalMessage = rawMessage.toUpperCase();
+
+const lastfourDigits = '123';
+const maskedAccountNumber = lastfourDigits.padStart(16,'*');
+console.log(maskedAccountNumber);
+
+const templateStrings = `last four digit of my account is`.concat(' ',lastfourDigits);
+const capitalMessages = message.toUpperCase()
+const bankBalance = 9873;
+
+const templateString2 = `I have ₹${bankBalance} in my account.`;
+
+const addedString = 'I have ₹' + bankBalance + ' in my account.';
