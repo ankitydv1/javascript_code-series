506f6d842c362d97494f1ab1a3442284cf372fa0
Ankit Yadav <khushhal2112@gmail.com>
2024-03-07 18:49:25 +0000

    conatiner config done

---
+++ 07_mathObject/script.js
@@ -0,0 +1,5 @@
+const width = prompt('pls enter Rectangle width');
+const height = prompt('pls enter Rectangle height');
+
+console.log(`area of rectangle is :${width*height}`);
+document.write(`area of rectangle is:${width * height}`);
