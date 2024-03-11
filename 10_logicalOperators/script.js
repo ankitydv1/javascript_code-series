3dbd1e549037be348ade2a237da31a3cf335058e
Ankit Yadav <khushhal2112@gmail.com>
2024-03-11 08:42:39 +0000

    container config done

---
+++ 10_logicalOperators/script.js
@@ -0,0 +1,9 @@
+const userAge = 18;
+
+const isCollegeStudent = (userAge >=17)&&(userAge <= 22);
+const isSchoolStudent = (userAge >= 2) && (userAge <= 19);
+
+const isStudent  = (isSchoolStudent) || (isCollegeStudent)
+
+const andResult = 0 && 2;
+const orResult = 0 || 2;
