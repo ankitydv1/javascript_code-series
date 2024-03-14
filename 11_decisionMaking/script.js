18e9d854f368c79b46a34015a4632f32e50a8989
Ankit Yadav <khushhal2112@gmail.com>
2024-03-14 16:52:03 +0000

    container config done

--- 11_decisionMaking/script.js
+++ 11_decisionMaking/script.js
@@ -1,5 +1,28 @@
-const userAge = 28
-const userName ='akash'
+const username = prompt('please enter your name');
+const userage = parseInt(prompt('please enter your age'));
 
-console.log(useerName);
-console.log(userAge)
+console.log(`name: ${username}`);
+console.log(`age: ${userage}`);
+
+if(userage>=1 && userage<5){
+    console.log('user is kid');
+    console.log('just chill');
+}
+if(userage>=5 && userage<=17){
+    console.log(`${username} is a school student`);
+    console.log('And he or she is learning science and maths');
+}
+if(userage>=18 && userage<=25){
+    console.log('user is college student');
+    console.log('he or she is learning data structure');
+}
+if(userage>25 && userage<=50){
+    console.log('user is working professinal');
+    console.log('he or she is working in IT');
+}
+if(userage>50){
+    console.log('user is working professinal');
+    console.log('he or she is working in it');
+}
+
+console.log('program is ended');
