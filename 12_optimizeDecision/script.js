1f3da157004102fdbe9d2cad92275847da824634
Ankit Yadav <khushhal2112@gmail.com>
2024-03-16 16:07:02 +0000

    container config done

--- 12_optimizeDecision/script.js
+++ 12_optimizeDecision/script.js
@@ -1,2 +1,19 @@
 const username = prompt('please enter your name');
 const userage = prompt('please enter your age')
+
+console.log(`name: ${username}`);
+console.log(`age: ${userage}`);
+
+
+if(userage >= 0 && userage <= 4){
+    console.log(`${username} is a kid`);
+}
+else if(userage >= 5 && userage <= 17){
+    console.log(`${username} is a school student`);
+}
+else if(userage >= 18 && userage <= 24){
+    console.log(`${username} is a college student`);
+}
+console.log('program ended')
+
+
