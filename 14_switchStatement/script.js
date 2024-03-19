311f7831a7fcd8ea304781e046362767c2a68c2a
Ankit Yadav <khushhal2112@gmail.com>
2024-03-19 16:37:18 +0000

    container config done

---
+++ 14_switchStatement/script.js
@@ -0,0 +1,27 @@
+const dayNumber = 2
+
+if(dayNumber === 0){
+    console.log('it is sunday')
+}
+else if(dayNumber === 1){
+    console.log('its monday today')
+}
+else if(dayNumber === 2){
+    console.log('its tuesday today')
+}
+else if(dayNumber === 3){
+    console.log('its wednesday today')
+}
+else if(dayNumber === 4){
+    console.log('its thursday today')
+}
+else if(dayNumber === 5){
+    console.log('its friday today')
+}
+else if(dayNumber === 6){
+    console.log('its saturday today')
+}
+else{
+    console.log('enter a valid number')
+}
+
