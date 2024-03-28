aa070be26081f5f03d9566460a972de538b6be90
Ankit Yadav <khushhal2112@gmail.com>
2024-03-28 05:27:19 +0000

    container config done

--- 15_ternaryOperator/script.js
+++ 15_ternaryOperator/script.js
@@ -1,6 +1,10 @@
 
 const fe = 'f';
 
-const userMessage = `${gender === 'f' ? 'she' : 'he'} is a college student`;
+const userMessage = `${gender.toLocaleLowerCase === 'f' ? 'she' : 'he'} is a college student`;
 
 console.log(userMessage);
+
+const result = 0 ? 'ankit' :'yadav' ? '12' : 0
+console.log(result);
+ 
--- 16_seeVariable/script.js
+++ 16_seeVariable/script.js
@@ -1,2 +1,9 @@
 const name = 'see Variable how it works'
 console.log(name);
+
+const frstName = Ankit;
+const userAge = 23;
+const birthYr = 2005;
+const isgraduate = false;
+
+// in the memory in console
