9e93b8482badcd3a02e822cfebfd930cf2551edf
Ankit Yadav <khushhal2112@gmail.com>
2024-03-29 04:31:49 +0000

    container config done

--- 17_objects/script.js
+++ 17_objects/script.js
@@ -1,3 +1,4 @@
+
 const myProfile = {
     firstName:'ankit',
     lastName :'yadav',
@@ -6,3 +7,27 @@ const myProfile = {
 
 console.log(myProfile.firstName);
 console.log(myProfile.lastName);
+
+const user1 = {
+    firstName : 'Ankita'
+}
+const user2 = {
+    firstName :'nikk', 
+    lastName : 'yadav',
+    address: {
+        city: 'ballia',
+        state: 'UP',
+        pinCode : 277209,
+        moreDetails : {
+            locality : 'milestoneMall',
+            subLocality : 'NCC'
+        }
+   }
+}
+console.log(user2.address);
+console.log()
+
+// console.log(user2.firstName);
+// console.log(user2["lastName"]);
+
+
