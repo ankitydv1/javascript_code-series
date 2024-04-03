d77b0f537bb6c3ddd2fc83907eeb70af1758f455
Ankit Yadav <khushhal2112@gmail.com>
2024-04-03 06:10:32 +0000

    container config done

--- 22_array/script.js
+++ 22_array/script.js
@@ -1,1 +1,14 @@
 const fruit = ['mango','apple','banana','guava']
+
+const myFruits = fruits
+
+myFruits.push('Dates')
+myFruits.push('Grapes')
+
+const user1 = {
+    firstName : 'anurag',
+    lastName :
+}
+
+
+
