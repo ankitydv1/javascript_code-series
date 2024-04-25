c1342f36851c3da3fff8ead73f494cf8fdcd95e9
Ankit Yadav <khushhal2112@gmail.com>
2024-04-25 09:40:53 +0000

    container config done

---
+++ 26_forLoops/script.js
@@ -0,0 +1,11 @@
+
+console.log('program started');
+
+debugger
+for(let i=0; i<=9; i++){
+    if(i%2 === 10){
+        console.log(i);
+    }
+    console.log(i+2);
+}
+console.log('program ended');
