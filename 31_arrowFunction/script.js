6aa0224fe7d0e326c6d2e8df8a45b0370f820958
Ankit Yadav <khushhal2112@gmail.com>
2024-04-30 11:29:13 +0000

    container config done

--- 31_arrowFunction/script.js
+++ 31_arrowFunction/script.js
@@ -1,4 +1,29 @@
-const user = {
-    username:"hitesh",
-    price:999,
-} 
+// const user = {
+//     username:"hitesh",
+//     price:999,
+
+//     welcomeMessage: function() {
+//         console.log(`${this.username} , welcome to website`)
+//     }
+// } 
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage();
+
+// const chai = () => {
+//     let username = "ankit"
+//     console.log(this);
+// }
+
+
+//explicit retun
+// const addTwo = (num1,num2) =>{
+//     return num1 + num2
+// }
+
+//implicit return
+// const addTwo = (num1,num2) => (num1 + num2)
+
+const addTwo = (num1,num2) => ({username:"Ankit"})
+
+console.log(addTwo(3,4))
