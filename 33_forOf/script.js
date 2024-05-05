3a55fe498f269cce3e1d4a902099bbe27f76a150
Ankit Yadav <khushhal2112@gmail.com>
2024-05-05 16:11:24 +0000

    container config done

--- 33_forOf/script.js
+++ 33_forOf/script.js
@@ -9,3 +9,24 @@ const greeting  = "hello world";
 for(const greet of greeting){
     console.log(greet);
 }
+
+//maps
+
+const map = new Map()
+map.set('IN', "INDIA")
+map.set('USA', "United States Of America")
+
+console.log(map);
+
+for(const [key, value] of map){
+    console.log(key, ':-',value);
+}
+
+const myObject = {
+    'game1' : 'NFS',
+    'game2' : 'Spiderman'
+}
+
+for(const [key, value] of myObject){
+    console.log(key,':-', value);
+}
