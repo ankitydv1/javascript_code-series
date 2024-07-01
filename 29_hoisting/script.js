7a3c9405246599393b3c87073163456d169fd7d0
Ankit Yadav <khushhal2112@gmail.com>
2024-07-01 20:34:30 +0000

    container config done

--- 29_hoisting/script.js
+++ 29_hoisting/script.js
@@ -1,5 +1,19 @@
 
 
 console.log(a);
-var a;
+var a = 'ankit';
 // const  a = 'ankit'
+
+hi()
+//function definition
+//function declaration
+function hi() {
+    console.log('hello');
+}
+
+sayHi()
+//function definition
+//function expression
+const sayHi = function(){
+    console.log('hello');
+}
