c0dd5f820d1ab30822aab03e434b9aa5d7b6c265
Ankit Yadav <khushhal2112@gmail.com>
2024-07-29 12:09:55 +0000

    container config done

---
+++ 35_setTimeout/script.js
@@ -0,0 +1,19 @@
+// const timer1 = setTimeout(`console.log("hii1")`,1000)
+// const timer2 = setTimeout(`console.log("hii2")`,0)
+// const timer3 = setTimeout(hello,3000, 'akj' ,99,3000)
+
+// clearTimeout(timer2)
+
+const timer1 = setInterval(`console.log("hii1")`,1000)
+const timer2 = setInterval(`console.log("hii2")`,0)
+const timer3 = setInterval(hello,3000, 'akj' ,99,3000)
+
+//clearInterval(timer1)
+
+
+function hello(){
+    //console.log(arguments[2]);
+    console.log("hello world");
+}
+
+console.log('hijjiiiiii');
